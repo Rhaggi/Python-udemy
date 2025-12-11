d3c67b306712808ba0d65d4dce9409b104b293bd
rosahaggi <rosahaggi@gmail.com>
2025-12-11 09:54:59 -0300

    graficos

---
+++ graficos/grafico.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+eixo_x_dias = [1,5,10,15,20,25,30]
+
+eixo_y_temp_max = [28,29,25,32,34,36,31]
+eixo_y_temp_min = [21,22,17,23,23,24,20]
+
+plt.title('Temperaturas macximas e minimas')
+
+plt.xlabel('Datas')
+plt.ylabel('Temperatura')
+
+plt.plot(eixo_x_dias, eixo_y_temp_max)
+plt.plot(eixo_x_dias, eixo_y_temp_min)
+
+plt.legend(['Temp max', 'Temp min'])
+
+plt.show()
